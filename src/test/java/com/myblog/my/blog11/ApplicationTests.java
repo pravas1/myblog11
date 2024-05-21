e2182b592356ecafe937d320dad923b6345fe952
pravas <pravasroutray2@gmail.com>
2024-05-22 00:55:59 +0530

    127:create save post feature

---
+++ src/test/java/com/myblog/my/blog11/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog.my.blog11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
